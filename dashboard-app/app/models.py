c647ee59a404ee4e38713e698fa80c0accbf16fd
marcelospit1 <marcespit27@gmail.com>
2026-05-14 00:00:24 -0400

    CRUD categoria producto mas mostrar imagen y corregido las fechas

--- dashboard-app/app/models.py
+++ dashboard-app/app/models.py
@@ -1,12 +1,88 @@
+from datetime import datetime, UTC
+from sqlalchemy import ForeignKey
+from markupsafe import Markup
 from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, Numeric, String, Text, Boolean, DateTime
 from sqlalchemy.orm import relationship
 
-"""
 
-You can use the extra Flask-AppBuilder fields and Mixin's
+class Categoria(Model):
+    __tablename__ = 'categoria'
 
-AuditMixin will add automatic timestamp of created and modified by who
+    id = Column(Integer, primary_key=True)
+
+    nombre = Column(String(100), nullable=False)
+    descripcion = Column(Text, nullable=True)
+    imagen = Column(String(255), nullable=True)
+
+    estado = Column(Boolean, default=True, nullable=False)
+
+    creado_en = Column(
+        DateTime,
+        default=lambda: datetime.now(UTC),
+        nullable=False
+    )
+
+    actualizado_en = Column(
+        DateTime,
+        default=lambda: datetime.now(UTC),
+        onupdate=lambda: datetime.now(UTC),
+        nullable=False
+    )
 
 
-"""
+    productos = relationship(
+        'Producto',
+        back_populates='categorias'
+    )
+    def imagen_preview(self):
+    
+        if self.imagen:
+
+            return Markup(
+                f'<img src="/static/{self.imagen}" width="100">'
+            )
+
+        return "Sin imagen"
+
+    def __repr__(self):
+        return self.nombre
+    
+class Producto(Model):
+    __tablename__ = 'producto'
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+    descripcion = Column(Text, nullable=True)
+    precio = Column(Numeric(10, 2), nullable=False)
+    categoria_id = Column(Integer, ForeignKey('categoria.id'), nullable=False)
+    imagen = Column(String(255), nullable=True)
+    estado = Column(Boolean, default=True, nullable=False)
+
+    creado_en = Column(
+        DateTime,
+        default=lambda: datetime.now(UTC),
+        nullable=False
+    )
+
+    actualizado_en = Column(
+        DateTime,
+        default=lambda: datetime.now(UTC),
+        onupdate=lambda: datetime.now(UTC),
+        nullable=False
+    )
+    categorias = relationship(
+        'Categoria',
+        back_populates='productos'
+    )
+    def imagen_preview(self):
+    
+        if self.imagen:
+
+            return Markup(
+                f'<img src="/static/{self.imagen}" width="100">'
+            )
+
+        return "Sin imagen" 
+    def __repr__(self):
+        return self.nombre
--- dashboard-app/app/views.py
+++ dashboard-app/app/views.py
@@ -1,43 +1,57 @@
-from flask import current_app, render_template
+import os
+from flask import current_app, request
+from werkzeug.utils import secure_filename
+from wtforms import FileField
+from .extensions import appbuilder, db
+from flask_appbuilder import ModelView
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from .models import Categoria, Producto
+class CategoriaModelView(ModelView):
+    datamodel = SQLAInterface(Categoria)
+    add_form_extra_fields = {
+    "imagen_file": FileField("imagen")
+    }
+    label_columns = {'nombre': 'Nombre', 'descripcion': 'Descripción', 'estado': 'Estado', 'creado_en': 'Creado En', 'actualizado_en': 'Actualizado En','imagen_preview':'Imagen'}
+    list_columns = ['nombre', 'descripcion', 'estado', 'creado_en','imagen_preview']
+    add_columns = ['nombre', 'descripcion','imagen_file', 'estado']
+    edit_columns = ['nombre', 'descripcion', 'imagen', 'estado']
+    show_columns = ['nombre', 'descripcion', 'imagen', 'estado', 'creado_en', 'actualizado_en']
+    def pre_add(self, item):
+        file = request.files.get("imagen_file")
+        if file:
+            filename = secure_filename(file.filename)
+            upload_path = os.path.join(
+                current_app.root_path,
+                'static',
+                'uploads'
+            )
+            os.makedirs(upload_path, exist_ok=True)
+            file_path = os.path.join(upload_path, filename)
+            file.save(file_path)
+            item.imagen = f'uploads/{filename}'
+class ProductoModelView(ModelView):
+    add_form_extra_fields = {
+        "imagen_file": FileField("imagen")
+    }
+    datamodel = SQLAInterface(Producto)
+    label_columns = {'nombre': 'Nombre', 'descripcion': 'Descripción', 'precio': 'Precio', 'categorias': 'Categoría', 'estado': 'Estado', 'creado_en': 'Creado En', 'actualizado_en': 'Actualizado En','imagen_preview':'Imagen'}
+    list_columns = ['nombre', 'precio', 'categorias', 'estado','imagen_preview']
+    add_columns = ['nombre', 'descripcion','precio','categorias','estado','imagen_file']
+    edit_columns = ['nombre', 'descripcion','precio','categorias','estado']
+    show_columns = ['nombre', 'descripcion','precio', 'imagen','estado','creado_en','actualizado_en']
+    def pre_add(self, item):
+        file = request.files.get("imagen_file")
+        if file:
+            filename = secure_filename(file.filename)
+            upload_path = os.path.join(
+                current_app.root_path,
+                'static',
+                'uploads'
+            )
+            os.makedirs(upload_path, exist_ok=True)
+            file_path = os.path.join(upload_path, filename)
+            file.save(file_path)
+            item.imagen = f'uploads/{filename}'
+appbuilder.add_view(CategoriaModelView, "Categorías", icon="fa-folder-open-o", category="Configuraciones", category_icon="fa-cogs")
 
-"""
-    Create your Model based REST API::
-
-    class MyModelApi(ModelRestApi):
-        datamodel = SQLAInterface(MyModel)
-
-    appbuilder.add_api(MyModelApi)
-
-
-    Create your Views::
-
-
-    class MyModelView(ModelView):
-        datamodel = SQLAInterface(MyModel)
-
-
-    Next, register your Views on create_app Flask factory::
-
-
-    appbuilder.add_view(
-        MyModelView,
-        "My View",
-        icon="fa-folder-open-o",
-        category="My Category",
-        category_icon='fa-envelope'
-    )
-"""
-
-"""
-    Application wide 404 error handler
-"""
-
-
-@current_app.errorhandler(404)
-def page_not_found(e):
-    return (
-        render_template(
-            "404.html", base_template=appbuilder.base_template, appbuilder=appbuilder
-        ),
-        404,
-    )
+appbuilder.add_view(ProductoModelView, "Productos", icon="fa-cube", category="Configuraciones", category_icon="fa-cogs")
